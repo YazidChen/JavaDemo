95a9862f0351da7efa28077c67608f15401d8940
陈泽升 <chenzesheng@forchange.tech>
2020-06-03 09:07:14 +0800

    import springboot

---
+++ src/main/java/com/yazid/demo/AppApplication.java
@@ -0,0 +1,16 @@
+package com.yazid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author YazidChen
+ * @date 2017-12-07
+ */
+@SpringBootApplication
+public class AppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppApplication.class, args);
+    }
+}
